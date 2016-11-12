2e168404ff787459958c42304e5291632c1d85dc
Ethan <itsmeortheethan@gmail.com>
2016-11-11 20:11:42 -0800

    timer

--- timer.js
+++ timer.js
@@ -1,19 +1,31 @@
-  
-   window.onload = function(){
-     var hou = 2;
-     var sec = 60;
-     setInterval(function(){
+var running = false;
+var min = 0;
+var sec = 0;
 
-       document.getElementById("mock-timer").innerHTML = hou +" : " + sec ;
-       sec;
-       if(sec == 00)
-       {
-         hou;
-         sec = 60;
-         if (hou == 0)
-         {
-            hou = 2;
-         }
-       }
-      },500);
-    }
+function start() {
+
+	if (!running) {
+		running = true;
+		var interval = setInterval(function() {
+			sec++;
+			if (sec < 10) {
+				document.getElementById("timer").innerHTML = min + " : 0" + sec;
+			} else if (sec > 60) {
+				sec = 0
+				min++;
+			} else {
+				document.getElementById("timer").innerHTML = min + " : " + sec;
+			}
+
+		}, 1000);
+	}
+
+}
+
+function reset() {
+	running = false;
+	min = 0;
+	sec = 0;
+	clearInterval(interval);
+
+}
